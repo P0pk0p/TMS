04da6e11384882984a065b9bdbd88e469f1813a9
P0pk0p <gglobal7774@gmail.com>
2022-11-21 17:22:07 +0300

    “lesson1”

---
+++ lesson1_1.py
@@ -0,0 +1,51 @@
+import requests
+# Задания_1
+a = 2
+b = 2
+answer = a + b
+answer_2 = a - b
+answer_3 = a / b
+answer_4 = a * b
+answer_5 = a ** b
+answer_6 = a % b
+answer_7 = a // b
+print(answer)
+print(answer_2)
+print(answer_3)
+print(answer_4)
+print(answer_5)
+print(answer_6)
+print(answer_7)
+
+# Задания_2
+a1 = ((17 / 2) * 3) + 2
+a2 = 2 + (17 / 2) * 3
+a3 = (19 % 4) + (15 / 2 * 3)
+a4 = (15 + 6) - (10 * 4)
+a5 = (17 / 2) % 2 * (3**3)
+print(a1,a2,a3,a4,a5)
+
+
+# Задания_3
+x = 5
+y = 2
+an = (x - y) / 1 + (x * y)
+print(an)
+
+# Задания_1
+my_str = "aaaaaBccBddBeeBffBggB"
+print (len(my_str))
+my_str =  my_str[5:5] + my_str[8:8] + my_str[11:11] + my_str[14:14] + my_str[17:17]
+print(my_str)
+
+#Задания_2
+my_str_2 = "AAAABBBBCCCCDDDDFFFF"
+print (len(my_str_2))
+my_str_2 = my_str_2 [0:4] + my_str_2 [4:8] + my_str_2 [8:12]+ my_str_2 [12:16] + my_str_2 [16:20]
+print(my_str_2)
+
+#Задания_3
+my_str_3 = "PYTHON"
+my_str_3 = my_str_3[::-1]
+print(my_str_3)
+
---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+n4 = 3
+n5 = 3
+n6 = 3
+
+n8 = 3
+n8 = 3
+
+print(n8)
