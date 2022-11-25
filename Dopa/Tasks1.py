5fbee8792bd5e88526583e7135bc9bab2e3de132
P0pk0p <gglobal7774@gmail.com>
2022-11-26 01:00:53 +0300

    lesson3

--- Dopa/Tasks1.py
+++ Dopa/Tasks1.py
@@ -2,11 +2,13 @@ import requests
 
 sequence = range(1, 11)
 
-result = [+2 for x in sequence]
+result = [x for x in sequence]
 
 # result = []
 # for x in sequence:
-#      result.append(x)
+#     result.reverse(x)
 
 
 print(result)
+ re = [1,2,3,4,5]
+ print (int(re.reverse()))
